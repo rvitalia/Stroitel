dd91e9af62edd597b535000d6b1cdce0f014ab41
rvitalia <riabinines@yandex.ru>
2023-04-14 22:20:25 +0200

    hero page

--- src/assets/js/firstpage/app.js
+++ src/assets/js/firstpage/app.js
@@ -1,34 +1,22 @@
-import Swiper, { Pagination, Navigation } from 'swiper';
+// import Swiper, { Pagination, Navigation } from 'swiper';
 
-Swiper.use([Pagination]);
+// Swiper.use([Pagination]);
 
-const swiperhero = new Swiper('.swiper__hero', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: 'true',
-        type: 'bullets',
-    },
-});
+// const swiperhero = new Swiper('.swiper__hero', {
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: 'true',
+//         type: 'bullets',
+//     },
+// });
 
-const swiperadv = new Swiper('.swiper__adventages', {
-    modules: [Navigation],
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
+// const swiperadv = new Swiper('.swiper__adventages', {
+//     modules: [Navigation],
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+// });
 
 
 
-// export function search() {
-//     let search = document.querySelector('#header__search');
-//     console.log(search);
-//     let widgets = document.querySelector('#widgets');
-//     console.log(widgets);
-
-//     search.addEventListener('click', () => {
-//         widgets.classList.toggle('active__input');
-//         console.log(widgets);
-//     })
-// }
-// search();
